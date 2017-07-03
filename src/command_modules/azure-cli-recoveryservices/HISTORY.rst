ccab97a3ed9196be2cd56abdf1ee95ef24ee2a85
Samuel Anudeep <anudeeb@microsoft.com>
2017-07-03 21:03:29 +0530

    Removing recoveryservices (duplicate) module

--- src/command_modules/azure-cli-recoveryservices/HISTORY.rst
+++
@@ -1,9 +0,0 @@
-.. :changelog:
-
-Release History
-===============
-
-0.0.1b1 (YYYY-MM-DD)
-^^^^^^^^^^^^^^^^^^^^
-
-* Initial release of module.
--- src/command_modules/azure-cli-recoveryservices/azure/cli/command_modules/recoveryservices/_client_factory.py
+++
@@ -1,8 +0,0 @@
-def _recoveryservices_client_factory(**_):
-    from azure.mgmt.recoveryservices import RecoveryServicesClient
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-
-    return get_mgmt_service_client(RecoveryServicesClient)
-
-def vaults_mgmt_client_factory(_):
-    return _recoveryservices_client_factory().vaults
